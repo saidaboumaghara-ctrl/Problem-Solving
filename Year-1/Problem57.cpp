93c8ad2ff1c6e69e70ea2e508b6780913808027c
saidabou <saidaboumaghara@gmail.com>
2026-06-05 01:13:54 +0300

    Add Problem57.cpp to read and display vowels
    Implement a program that reads a string and outputs the vowels with spaces.

---
+++ Year-1/Problem57.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<ctime>
+#include <iomanip>
+#include<vector>
+#include <set>
+#include<cmath>
+#include <string>
+using namespace std;
+string Readastring();
+void spaceatstringtovowels(string text);
+int main()
+{
+    string nam = Readastring();
+    spaceatstringtovowels(nam);
+
+    return 0;
+}
+string Readastring()
+{
+    string text;
+    cout << "Please enter your strings\n";
+    getline(cin, text);
+
+
+    return text;
+
+}
+void spaceatstringtovowels(string text)
+{
+
+    
+    cout << " vowels is : ";
+    for (int i = 0; i < text.length(); i++)
+    {
+        if ('i' == char(tolower(text[i])) || 'a' == char(tolower(text[i])) || 'o' == char(tolower(text[i])) || 'u' == char(tolower(text[i])) || 'e' == char(tolower(text[i])))
+            cout << text[i] << " ";
+
+   
+    }
+   
+}
